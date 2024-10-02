f04078ed6eed690729d2180c50b430ef65173ef0
shida-zheng-ICD2O1-01 <shida.zheng@student.tdsb.on.ca>
2024-10-02 14:03:59 +0000

    a

--- PP1_10.py
+++ PP1_10.py
@@ -1,24 +1,35 @@
 
 
+import math
+
+
 def q1(): 
   #Write Assignment code here
-
+  a = float(input("Inter a number:"))
+  print(math.sqrt(a))
 
 def q2(): 
   #Write Assignment code here
-
-
+  num = input("Inter a number:")
+  num1 = int(num)
+  print(math.isqrt(num1))
 def q3(): 
   #Write Assignment code here
-
+  a = float(input("Inter a number:"))
+  print(math.floor(a))
 
 def q4(): 
   #Write Assignment code here
-
+  a = float(input("Input a number:"))
+  print(math.ceil(a))
 
 def q5(): 
   #Write Assignment code here
-
+  a = float(input("Input a number:"))
+  b = float(input("Input another number:"))
+  c = a * b
+  d = c / 2
+  print(math.floor(d))
 
 
 #Do not alter the following code
